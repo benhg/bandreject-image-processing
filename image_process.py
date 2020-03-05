591cd1450bcc069800d7c4f30ce9977b799d904d
Ben Glick <glick@glick.cloud>
2020-03-05 11:10:43 -0800

    adding working filter

--- image_process.py
+++ image_process.py
@@ -4,45 +4,60 @@ import matplotlib.pyplot as plt
 from pylab import *
  
 im = imread('nasa.png')
+im1 = np.zeros((im.shape[0], im.shape[1]))
+print(im.shape, im1.shape)
+for i in range(im.shape[0]):
+    for j in range(im.shape[1]):
+        im1[i,j] = im[i,j]
  
-
-xs = im.shape[0]
-ys = im.shape[1]
+def padwithzeros(vector, pad_width, iaxis, kwargs):
+    vector[:pad_width[0]] = 0
+    vector[-pad_width[1]:] = 0
+    return vector
+ 
 # the LPF kernel
+kernel = [[1]*im.shape[1]]*im.shape[0]
 
-kernel = [[0]*xs]*ys
-for i,x in enumerate(kernel):
-    for j,y in enumerate(x):
-        if abs((5*i)**2 + (3*j)**2 - 1**2) < 5**2:
-            kernel[i][j] = 1000000
-            print("found point")
-
- 
 plt.figure(figsize=(15,10))
 plt.gray() # show the filtered result in grayscale
- 
-freq = fp.fft2(im)
+
+freq = fp.fft2(im1)
 freq_kernel = fp.fft2(fp.ifftshift(kernel))
-#freq_LPF = freq*freq_kernel # by the Convolution theorem
-#im2 = fp.ifft2(freq_LPF)
-#freq_im2 = fp.fft2(im2)
+for i, x in enumerate(freq_kernel):
+    for j,y in enumerate(x):
+        freq_kernel[i][j] = 1+0j
+import math
+# draw the circle
+for angle in range(0, 3600, 1):
+    angle = angle/10
+    for r in range(1100,1300):
+        r = r/10
+        x = r * math.sin(math.radians(angle)) + 0
+        y = r * math.cos(math.radians(angle)) + 0
+        freq_kernel[int(round(y))][int(round(x))] = 0j
+
+
+
+freq_LPF = freq*freq_kernel # by the Convolution theorem
+im2 = fp.ifft2(freq_LPF)
+freq_im2 = fp.fft2(im2)
  
 plt.subplot(2,3,1)
 plt.imshow(im)
 plt.title('Original Image', size=20)
 plt.subplot(2,3,2)
-plt.imshow(im)
+plt.imshow(im1)
 plt.title('Padded Image', size=20)
 plt.subplot(2,3,3)
 plt.imshow( (20*np.log10( 0.1 + fp.fftshift(freq))).astype(int), cmap='jet')
 plt.title('Original Image Spectrum', size=20)
 plt.subplot(2,3,4)
-plt.imshow( kernel)#(20*np.log10( 0.1 + fp.fftshift(kernel))).astype(int), cmap='jet')
-plt.title('Image Spectrum of the LPF', size=20)
+plt.imshow( (20*np.log10( 0.1 + fp.fftshift(freq_kernel))).astype(int), cmap='jet')
+plt.title('Image Spectrum of the Filter', size=20)
 plt.subplot(2,3,5)
-#plt.imshow( (20*np.log10( 0.1 + fp.fftshift(freq_im2))).astype(int), cmap='jet')
-plt.title('Image Spectrum after LPF', size=20)
+plt.imshow( (20*np.log10( 0.1 + fp.fftshift(freq_im2))).astype(int), cmap='jet')
+plt.title('Image Spectrum after Filtering', size=20)
 plt.subplot(2,3,6)
-#plt.imshow(im2.astype(np.uint8)) # the imaginary part is an artifact
+plt.imshow(im2.astype(np.float64)) # the imaginary part is an artifact
 plt.title('Output Image', size=20)
 plt.show()
